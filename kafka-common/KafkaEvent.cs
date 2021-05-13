fb98d0b0cd41ce97ad8e29834a7ac916a6a3fc7f
the-sharp-end <>
2021-05-13 11:39:34 -0500

    Added common project

---
+++ kafka-common/KafkaEvent.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace Kafka.Common
+{
+    public class KafkaEvent
+    {
+        public KafkaEvent(string message)
+        {
+            EventId = Guid.NewGuid();
+            EventTimestamp = DateTimeOffset.UtcNow;
+            Message = message;
+        }
+
+        protected KafkaEvent() { }
+
+        public Guid EventId { get; set; }
+        public DateTimeOffset EventTimestamp { get; set; }
+        public string Message { get; set; }
+
+        public static KafkaEvent HealthCheckEvent()
+        {
+            return new KafkaEvent("Health Check event");
+        }
+    }
+}
---
+++ kafka-common/KafkaOptions.cs
@@ -0,0 +1,10 @@
+﻿namespace Kafka.Common
+{
+    public class KafkaOptions
+    {
+        public string BootstrapServers { get; set; }
+        public string ClientId { get; set; }
+        public string GroupId { get; set; }
+        public string Topic { get; set; }
+    }
+}
